f0f8d9d7cdd7f0c5bc41e684dc028493310d24fc
nothiaki <celsohenrique367@gmail.com>
2024-06-12 13:58:26 -0300

    init

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		local neotree_open = false
+
+		local function toggle_neotree()
+			if neotree_open then
+				vim.cmd("Neotree close")
+				neotree_open = false
+			else
+				vim.cmd("Neotree filesystem reveal right")
+				neotree_open = true
+			end
+		end
+
+		vim.keymap.set("n", "<leader>e", toggle_neotree, {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+	end,
+}
