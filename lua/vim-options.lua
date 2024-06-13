6b08d555c848ca0f5723407e4eb674b0868a3b0f
nothiaki <celsohenrique367@gmail.com>
2024-06-12 23:37:56 -0300

    feat: harpoon plugin

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -15,4 +15,4 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>nh', ':nohlsearch<CR>')
