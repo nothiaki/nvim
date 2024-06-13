9de510ff220293ee6b6bab7ea017cc4e80bf38d5
nothiaki <celsohenrique367@gmail.com>
2024-06-12 22:22:41 -0300

    chore: ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,9 +27,10 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>f", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers , {})
 
       require("telescope").load_extension("ui-select")
     end,
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
+vim.o.showmode = false
 vim.g.mapleader = " "
 
 vim.opt.swapfile = false
