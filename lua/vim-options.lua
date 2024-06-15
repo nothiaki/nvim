a56bd62e49f45339e94515e4e668ff768893bc83
nothiaki <celsohenrique367@gmail.com>
2024-06-15 20:52:16 -0300

    feat: mini.pairs

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
+vim.cmd("set signcolumn=no")
 vim.o.showmode = false
 vim.g.mapleader = " "
 
